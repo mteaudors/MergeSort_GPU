f80db6c5e88c113a32f32be262b13534360aae70
YValeri <yoannvaleri@netcourrier.com>
2019-12-03 21:44:40 +0100

    Ajout du PCR, normalisation des écritures

--- Thomas_PCR.cu
+++ Thomas_PCR.cu
@@ -3,383 +3,167 @@
 #include <stdlib.h>
 
 #define EPS (0.0000001f)
-
 #define M (4*1048576)
-__device__ int gl[M];
 
+__device__ float Cp[M];
 
-__device__ float Cp[M]; 
+// Produces tridiagonal symmetric diagonally dominant matrices
+__global__ void Tri_k(float *sub, float *diag, float norm, int i, int n, int total_length)
+{
+	// Identifies the thread working within a group
+	int thread_grp_idx = threadIdx.x%n;
+	// Identifies the data concerned by the computations
+	int data_range = (threadIdx.x - thread_grp_idx) / n;
+	// The global memory access index
+	int gb_index_x = data_range + blockIdx.x*(blockDim.x / n);
 
+	if (gb_index_x*n + thread_grp_idx + i < total_length) {
+		diag[gb_index_x*n + thread_grp_idx + i] = ((float)thread_grp_idx + 1.0f) / (norm);
+		if (thread_grp_idx > 0)
+			sub[gb_index_x*n + thread_grp_idx + i] = ((float)thread_grp_idx + 1.0f) / (norm * 3);
+		else
+			sub[gb_index_x*n + thread_grp_idx + i] = 0.0f;
+	}
+}
 
 // Thomas resolution for tridiagonal symmetric matrices
-__global__ void thom_sym_k(float *S, float *D, float *Y, int n){
-
+__global__ void thom_sym_k(float *sub, float *diag, float *Y, int n){
 	// The global memory access index
 	int idx = threadIdx.x + blockIdx.x*blockDim.x;
+
+	//Local variables for easier access and less calculation
 	int j;
+	int first = idx * n, first_c = first + 1;
+	int cur_first, cur_first_c;
+	int n_first = first + n - 1, n_first_c = first_c + n - 2;
 
-
-	int first = idx*n;
-	int first_c = idx*n + 1;
-
-	float d = D[first];
-	Cp[first_c] = S[first_c] / d; 
+	//First forward substitution
+	float d = diag[first];
+	Cp[first_c] = sub[first_c] / d;
 	Y[first] = Y[first] / d;
 
-	for (j=1;j<n-1;j++) { //Decomposition and forward substitution.
-		Cp[first_c + j] = S[first_c + j] / (D[first + j] - S[first_c + j - 1]*Cp[first_c + j - 1]);	
-		Y[first + j] = (Y[first + j] - S[first_c + j - 1]*Y[first + j - 1]) / (D[first + j] - S[first_c + j - 1]*Cp[first_c + j - 1]);
+	//Decomposition and forward substitution.
+	for (j=1;j<n-1;j++) {
+		cur_first = first + j;
+		cur_first_c = first_c + j;
+		Cp[cur_first_c] = sub[cur_first_c] / (diag[cur_first] - sub[cur_first_c - 1]*Cp[cur_first_c - 1]);
+		Y[cur_first] = (Y[cur_first] - sub[cur_first_c - 1]*Y[cur_first - 1]) / (diag[cur_first] - sub[cur_first_c - 1]*Cp[cur_first_c - 1]);
 	}
 
 	// One more iteration for Y (j=n-1)
-	Y[first + n - 1] = (Y[first + n - 1] - S[first_c + n - 2]*Y[first + n - 2]) / (D[first + n - 1] - S[first_c + n - 2]*Cp[first_c + n - 2]);
+	Y[n_first] = (Y[n_first] - sub[n_first_c]*Y[n_first - 1]) / (diag[n_first] - sub[n_first_c]*Cp[n_first_c]);
 
 	for (j=(n-2);j>=0;j--){ //Backsubstitution.
-		Y[first + j] = Y[first + j] - Cp[first_c + j]*Y[first + j + 1];
+		cur_first = first + j;
+		cur_first_c = first_c + j;
+		Y[cur_first] = Y[cur_first] - Cp[cur_first_c]*Y[cur_first + 1];
 	}
+
 }
 
-///////////////////////////////////////////////////////////////////////
-// Parallel cyclic reduction for tridiagonal symmetric matrices
-///////////////////////////////////////////////////////////////////////
-__global__ void pcr_sym_k(float *a, float *b, float *y, int n)
-{
+__global__ void pcr_sym_k(float *sub, float *diag, float *Y, float *X, int n){
 	// Identifies the thread working within a group
-	int tidx = threadIdx.x%n;
+	int thread_grp_idx = threadIdx.x%n;
 	// Identifies the data concerned by the computations
-	int Qt = (threadIdx.x - tidx) / n;
+	int data_range = (threadIdx.x - thread_grp_idx) / n;
 	// The global memory access index
-	int gb_index_x = Qt + blockIdx.x*(blockDim.x / n);
-	printf("truc\n");
+	int gb_index_x = data_range + blockIdx.x*(blockDim.x / n);
 
-	int i;
-	int nt = 4 * Qt*n;
-	int d = (n / 2 + (n % 2))*(tidx % 2) + (int)tidx / 2;
+	//Local variables
+	int i, j;
+	int sh_range = 5 * data_range * n, s = 1;
+	int right_idx, left_idx;
+	int index = gb_index_x * n + thread_grp_idx;
+	float new_up_diag, new_diag, new_low_diag, new_res;
+	float tmp1, tmp2, tmp3;
+
 	// Shared memory
-	extern __shared__ float sAds[];
+	extern __shared__ float sh_mem[];
 
+	//Pointers to shared memory
+	float * sh_upper_diag = (float *)&sh_mem[sh_range];
+	float * sh_diag = (float *)&sh_upper_diag[n];
+	float * sh_lower_diag = (float *)&sh_diag[n];
+	float * sh_res = (float *)&sh_lower_diag[n];
+	float * sh_sol = (float *)&sh_res[n];
 
-	float *sa = (float*)&sAds[nt];
-	float *sb = (float*)&sa[n];
-	float *sy = (float*)&sb[n];
-	int *sl = (int*)&sy[n];
-
-	sa[tidx] = a[gb_index_x*n + tidx];
-	sb[tidx] = b[gb_index_x*n + tidx];
-	sy[tidx] = y[gb_index_x*n + tidx];
-	sl[tidx] = tidx;
-
-	if (threadIdx.x == 0) {
-		printf("Values : \n");
-		for (i = 0; i < 4 * 2 *n; ++i) {
-			printf("%.5e, ", sAds[i]);
-		}
-	}
-
-	int lL, aL, bL, yL, bLp, tl, tr; // local variables
-	float aLp, yLp;
+	sh_upper_diag[thread_grp_idx] = sub[index]; //sh_upper_diag will contain the values of the upper diagonal for the current group
+	sh_diag[thread_grp_idx] = diag[index]; //sh_diag will contain the values of the diagonal for the current group
+	sh_lower_diag[thread_grp_idx] = sub[index + 1]; //sh_lower_diag will contain the values of the lower diagonal for the current group, 
+	//meaning the sub diagonal from it's second index
+	sh_res[thread_grp_idx] = Y[index]; //sh_res will contain the values of the result vector, the Y vector
+	sh_sol[thread_grp_idx] = X[index]; //sh_sol will contain the values of the solution vector, the X vector
 
 	__syncthreads();
 
-	tl = tidx - 1;
-	tr = tidx + 1;
-	if (tl < 0) tl = 0;
-	if (tr >= n) tr = 0;
 
 	for (i = 0; i < (int)log2((float)n) + 1; i++) {
-		lL = sl[tidx];
-		aL = sa[tidx];
-		bL = sb[tidx];
-		yL = sy[tidx];
-		bLp = sb[tl];
 
+		j = thread_grp_idx;
 
-		if (fabsf(aL) > EPS) {
-			aLp = sa[tl];
-			yLp = sy[tl];
+		//Calculate the right and left index of the current cell
+		right_idx = j + s;
+		right_idx = right_idx & (n - 1);
+		left_idx = j - s;
+		left_idx = left_idx & (n - 1);
 
-			bL -= aL * aL / bLp;
-			yL -= aL * yLp / bLp;
-			aL = -aL * aLp / bLp;
-		}
+		//Calculate the pivots
+		tmp1 = sh_upper_diag[j] / sh_diag[left_idx];
+		tmp2 = sh_lower_diag[j] / sh_diag[right_idx];
 
-		aLp = sa[tr];
-		bLp = sb[tr];
-		if (fabsf(aLp) > EPS) {
-			yLp = sy[tr];
-			bL -= aLp * aLp / bLp;
-			yL -= aLp * yLp / bLp;
-		}
+		//Calculate the new values of the different component, modified by the pivots
+		new_diag = sh_diag[j] - sh_lower_diag[left_idx] * tmp1 - sh_upper_diag[right_idx] * tmp2;
+		new_res = sh_res[j] - sh_res[left_idx] * tmp1 - sh_res[right_idx] * tmp2;
+		new_up_diag = -sh_upper_diag[left_idx] * tmp1;
+		new_low_diag = -sh_lower_diag[right_idx] * tmp2;
 
 		__syncthreads();
-		if (i < (int)log2((float)n)) {//Permutation phase
-			sa[d] = aL;
-			sb[d] = bL;
-			sy[d] = yL;
-			sl[d] = lL;
-			__syncthreads();
-		}
-	}
 
-	sy[(int)tidx] = yL / bL;
-	__syncthreads();
+		//Permutate the current values with the new one, modified with pivot
+		sh_diag[j] = new_diag;
+		sh_res[j] = new_res;
+		sh_upper_diag[j] = new_up_diag;
+		sh_lower_diag[j] = new_low_diag;
 
-	float sum = 0.0f;
-	//Second matrix/vector product 
-	//Non-coalescent access to recover the solution
-	for (i = 0; i < n; i++) {
-		sum += sa[tidx*n + (int)sl[i]] * sy[i];
-	}
-	y[gb_index_x*n + tidx] = sum;
-}
-
-// Parallel cyclic reduction for tridiagonal symmetric matrices
-__global__ void pcr_sym_k3(float *a, float *b, float *y, int n)
-{
-	// Identifies the thread working within a group
-	int tidx = threadIdx.x%n;
-
-	// Identifies the data concerned by the computations
-	int Qt = (threadIdx.x - tidx) / n;
-
-	// The global memory access index
-	int gb_index_x = Qt + blockIdx.x*(blockDim.x / n);
-
-	int i; 
-	int nt = 4*Qt*n;
-	int d = (n / 2 + (n % 2))*(tidx % 2) + (int)tidx / 2;
-	
-	// Shared memory
-	extern __shared__ float sAds[];
-
-	float *sa = (float*)&sAds[nt];
-	float *sb = (float*)&sa[n];
-	float *sy = (float*)&sb[n];
-	int *sl = (int*)&sy[n];
-
-	sa[tidx] = a[gb_index_x*n + tidx];
-	sb[tidx] = b[gb_index_x*n + tidx];
-	sy[tidx] = y[gb_index_x*n + tidx];
-	sl[tidx] = tidx;
-
-	int lL, aL, bL, yL, bLp, tl, tr; // local variables
-
-	//////////////////////////////////////////////////////////////
-	//
-	//	Step 2:	Fill with your code : Additional variables definition 
-	//						  and copy the values in shared 
-	//
-	//////////////////////////////////////////////////////////////
-	// Local floats
-	float aLp, yLp;
-
-	/*sum = 0.0f;
-	for (i = 0; i < n; i++) {
-		sq[i*n + tidx] = q[gb_index_x*n2 + i * n + tidx];
-		sum += sq[i*n + tidx] * sy[i];
-	}
-	sy[tidx] = sum;*/
-	__syncthreads();
-
-	tl = tidx-1;
-	tr = tidx+1;
-	if(tl<0) tl=0;
-	if(tr>=n) tr=0;
-
-	for (i = 0; i < (int)log2((float)n) + 1; i++){
-		lL = sl[tidx];
-		aL = sa[tidx];
-		bL = sb[tidx];
-		yL = sy[tidx];
-		bLp = sb[tl];
-
-
-		if (fabsf(aL) > EPS) {
-			aLp = sa[tl];
-			yLp = sy[tl];
-
-			bL -= aL * aL / bLp;
-			yL -= aL * yLp / bLp;
-			aL = -aL * aLp / bLp;
-		}
-
-		aLp = sa[tr];
-		bLp = sb[tr];
-		if (fabsf(aLp) > EPS) {
-			yLp = sy[tr];
-			yL -= aLp * yLp / bLp;
-			bL -= aLp * aLp / bLp;
-		}
-
-		__syncthreads();
-		//Permutation phase
-		if (i < (int)log2((float)n)) {
-			sa[d] = aL;
-			sb[d] = bL;
-			sy[d] = yL;
-			sl[d] = (int)lL;
-			__syncthreads();
-		}
-	}
-
-	//////////////////////////////////////////////////////////////
-	// écrire la solution de shared vers la mémoire globale 
-	//////////////////////////////////////////////////////////////
-
-	sy[(int)tidx] = yL / bL;
-	__syncthreads();
-
-	float sum = 0.0f;
-	//Second matrix/vector product 
-	//Non-coalescent access to recover the solution
-	for (i = 0; i < n; i++) {
-		sum += sa[tidx*n + (int)sl[i]] * sy[i];
-	}
-	y[gb_index_x*n + tidx] = sum;
-}
-
-__global__ void pcr_sym_k2(float *q, float *a, float *b, float *y, int n)
-{
-	// Identifies the thread working within a group
-	int tidx = threadIdx.x%n;
-	// Identifies the data concerned by the computations
-	int Qt = (threadIdx.x - tidx) / n;
-	// The global memory access index
-	int gb_index_x = Qt + blockIdx.x*(blockDim.x / n);
-	// Local integers
-	int i, n2, nt, lL, d, tL, tR;
-	// Local floats
-	float aL, bL, yL, sum, aLp, bLp, yLp;
-	// Shared memory
-	extern __shared__ float sAds[];
-
-	n2 = n * n;
-	nt = Qt * n*(n + 4);
-	d = (n / 2 + (n % 2))*(tidx % 2) + (int)tidx / 2;
-
-	float* sq = (float*)&sAds[nt];
-	float* sa = (float*)&sq[n2];
-	float* sb = (float*)&sa[n];
-	float* sy = (float*)&sb[n];
-	int* sl = (int*)&sy[n];
-
-	sa[tidx] = a[gb_index_x*n + tidx];
-	sb[tidx] = b[gb_index_x*n + tidx];
-	sy[tidx] = y[gb_index_x*n + tidx];
-	sl[tidx] = tidx;
-	__syncthreads();
-
-	//First matrix/vector product
-	sum = 0.0f;
-	for (i = 0; i < n; i++) {
-		sq[i*n + tidx] = q[gb_index_x*n2 + i * n + tidx];
-		sum += sq[i*n + tidx] * sy[i];
-	}
-	__syncthreads();
-	sy[tidx] = sum;
-
-	//Left/Right indices of the reduction
-	tL = tidx - 1;
-	if (tL < 0) tL = 0;
-	tR = tidx + 1;
-	if (tR >= n) tR = 0;
-
-	for (i = 0; i < (int)log2((float)n) + 1; i++) {
-		lL = (int)sl[tidx];
-
-		aL = sa[tidx];
-		bL = sb[tidx];
-		yL = sy[tidx];
-
-		bLp = sb[tL];
-
-		//Reduction phase
-		if (fabsf(aL) > EPS) {
-			aLp = sa[tL];
-			yLp = sy[tL];
-
-			//bL = b[tidx] - a[tidx]*c[tidx]/b[tidx-1];
-			bL -= aL * aL / bLp;
-			//yL = y[tidx] - a[tidx]*y[tidx-1]/b[tidx-1];
-			yL -= aL * yLp / bLp;
-			//aL = -a[tidx]*a[tidx-1]/b[tidx-1];
-			aL = -aL * aLp / bLp;
-		}
-
-		aLp = sa[tR];
-		bLp = sb[tR];
-		if (fabsf(aLp) > EPS) {
-			yLp = sy[tR];
-			//bL -= c[tidx+1]*a[tidx+1]/b[tidx+1];
-			bL -= aLp * aLp / bLp;
-			//yL -= c[tidx+1]*y[tidx+1]/b[tidx+1];
-			yL -= aLp * yLp / bLp;
-		}
+		s <<= 1;
 		__syncthreads();
 
-		//Permutation phase
-		if (i < (int)log2((float)n)) {
-			sa[d] = aL;
-			sb[d] = bL;
-			sy[d] = yL;
-			sl[d] = (int)lL;
-			__syncthreads();
-		}
 	}
 
-	sy[(int)tidx] = yL / bL;
+	if (thread_grp_idx < s){
+		int addr = thread_grp_idx;
+		tmp3 = sh_diag[addr] * sh_diag[addr] - sh_lower_diag[addr] * sh_upper_diag[addr];
+		sh_sol[addr] = (sh_diag[addr] * sh_res[addr] - sh_lower_diag[addr] * sh_res[addr]) / tmp3;
+	}
+
 	__syncthreads();
 
-	sum = 0.0f;
-	//Second matrix/vector product 
-	//Non-coalescent access to recover the solution
-	for (i = 0; i < n; i++) {
-		sum += sq[tidx*n + (int)sl[i]] * sy[i];
-	}
-	y[gb_index_x*n + tidx] = sum;
+	//Move the final solution to the X vector
+	X[index] = sh_sol[thread_grp_idx];
 }
 
-
-// Produces tridiagonal symmetric diagonally dominant matrices 
-__global__ void Tri_k(float *D, float *S, float norm, int i, 
-						   int n, int L)
-{
-	// Identifies the thread working within a group
-	int tidx = threadIdx.x%n;
-	// Identifies the data concerned by the computations
-	int Qt = (threadIdx.x - tidx) / n;
-	// The global memory access index
-	int gb_index_x = Qt + blockIdx.x*(blockDim.x / n);
-
-	if(gb_index_x*n + tidx + i < L){
-		D[gb_index_x*n + tidx + i] = ((float)tidx+1.0f)/(norm);
-		if (tidx > 0){
-			S[gb_index_x*n + tidx + i] = ((float)tidx+1.0f)/(norm*3);
-		}else{S[gb_index_x*n + tidx + i] = 0.0f;}
-	}
-}
-
-
-int main(){
+int main() {
 
 	int i, j;
 
 	// The rank of the matrix
-	int Dim = 64;
-	
+	int Dim = 256;
+
 	// The number of blocks
-	int NB = M/Dim;
-	
+	int NB = M / Dim;
+
 	// The number of matrices to invert
 	int size = NB;
 
+	int minTB = 1024/Dim;
+
 	// The diagonal elements
-	float *D, *DGPU;
+	float *diag, *diagGPU;
 	// The subdiagonal elements
-	float *S, *SGPU;
+	float *sub, *subGPU;
 	// The system vector
 	float *Y, *YGPU;
-	float *A, *AGPU;
+	float *X, *XGPU;
 
 	float TimerV;					// GPU timer instructions
 	cudaEvent_t start, stop;		// GPU timer instructions
@@ -387,93 +171,62 @@ int main(){
 	cudaEventCreate(&stop);			// GPU timer instructions
 
 	// Memory allocation
-	A = (float *)calloc(size*Dim*Dim, sizeof(float));
-	D = (float *)calloc(size*Dim,sizeof(float));
-	S = (float *)calloc(size*Dim,sizeof(float));
-	Y = (float *)calloc(size*Dim,sizeof(float));
-	cudaMalloc(&AGPU, size*Dim*Dim * sizeof(float));
-	cudaMalloc(&DGPU, size*Dim*sizeof(float));
-	cudaMalloc(&SGPU, size*Dim*sizeof(float));
-	cudaMalloc(&YGPU, size*Dim*sizeof(float));
+	diag = (float *)calloc(size*Dim, sizeof(float));
+	sub = (float *)calloc(size*Dim, sizeof(float));
+	cudaMalloc(&diagGPU, size*Dim * sizeof(float));
+	cudaMalloc(&subGPU, size*Dim * sizeof(float));
+
+	X = (float *)calloc(size*Dim, sizeof(float));
+	Y = (float *)calloc(size*Dim, sizeof(float));
+	cudaMalloc(&XGPU, size*Dim * sizeof(float));
+	cudaMalloc(&YGPU, size*Dim * sizeof(float));
+
+	//Result vector
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < Dim; j++) {
+			Y[j + i * Dim] = 0.5f*j;
+		}
+	}
+	cudaMemcpy(YGPU, Y, size*Dim * sizeof(float), cudaMemcpyHostToDevice);
 
 	// Tridiagonal elements
-	int HM = M/(NB*Dim);
-	for (i=0; i*Dim*NB<M; i++){
-		Tri_k <<<NB,HM*Dim>>>(DGPU, SGPU, 10.0f, i*Dim*NB, Dim, 
-										  Dim*NB);
+	for (i = 0; i*Dim*NB < M; i++) {
+		Tri_k << <NB, Dim >> > (subGPU, diagGPU, 10.0f, i*Dim*NB, Dim, Dim*NB);
 	}
-
-	cudaMemcpy(D, DGPU, size*Dim*sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(S, SGPU, size*Dim*sizeof(float), cudaMemcpyDeviceToHost);
-
-	for (i = 0; i < Dim; ++i) {
-		for (j = 0; j < Dim; ++j) {
-			if (i == j)
-				A[i*Dim + j] = D[i];
-			else if (j - 1 == i)
-				A[i*Dim + j] = S[j - 1];
-			else if (j + 1 == i)
-				A[i*Dim + j] = S[j];
-			else
-				A[i*Dim + j] = 0.0f;
-		}
-	}
-
-	// Second member
-	for (i=0; i<size; i++){
-		for (j=0; j<Dim; j++){
-			Y[j+i*Dim]=0.5f*j;
-		}
-	}
-	cudaMemcpy(AGPU, A, size*Dim*Dim * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(YGPU,Y,size*Dim*sizeof(float),cudaMemcpyHostToDevice);
-
 
 	// Resolution part
-	cudaEventRecord(start,0);
+	cudaEventRecord(start, 0);
 
 
-	/////////////////////////////////////////////////////////////////////
-	// Step 2:	PCR
-	/////////////////////////////////////////////////////////////////////
-	// The minimum group of threads per block for PCR /!\ Has to be chosen by students
-	// /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\
-	
-	int minTB = 2;  // Choose the right value with respect to Dim
-	printf("%i \n", minTB);
-	//pcr_sym_k<<<NB/minTB, Dim*minTB, 4*minTB*Dim*sizeof(float)>>>(SGPU, DGPU, YGPU, Dim);
-	//pcr_sym_k2 << <NB, Dim*minTB, (minTB*Dim*(Dim + 4)) * sizeof(float) >> > (AGPU, SGPU, DGPU, YGPU, Dim);
-	
+	thom_sym_k << <NB / 256, 256 >> > (subGPU, diagGPU, YGPU, Dim);
+	//pcr_sym_k << <NB / minTB, Dim*minTB, 5 * minTB*Dim * sizeof(float) >> > (subGPU, diagGPU, YGPU, XGPU, Dim);
 
-	/////////////////////////////////////////////////////////////////////
-	// Step 1:	Thomas
-	/////////////////////////////////////////////////////////////////////
-	thom_sym_k<<<NB/256,256>>>(SGPU, DGPU, YGPU, Dim);
 
-	cudaEventRecord(stop,0);
+	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
-	cudaEventElapsedTime(&TimerV,start, stop);
+	cudaEventElapsedTime(&TimerV, start, stop);
 
-	cudaMemcpy(Y, YGPU, size*Dim*sizeof(float), cudaMemcpyDeviceToHost);
-	for (i=0; i<size; i++){
-	        if(i==573){
-			printf("\n\n");
-			for (j=0; j<Dim; j++){
-				printf("%.5e, ",Y[j+i*Dim]);
+	cudaMemcpy(X, YGPU, size*Dim * sizeof(float), cudaMemcpyDeviceToHost);
+	//cudaMemcpy(X, XGPU, size*Dim * sizeof(float), cudaMemcpyDeviceToHost);
+
+	for (i = 0; i < size; i++) {
+		if (i == 573) {
+			for (j = 0; j < Dim; j++) {
+				printf("%.5e, ", X[j + i * Dim]);
 			}
 		}
 	}
 
-
 	printf("Execution time: %f ms\n", TimerV);
 
+
 	// Memory free for other arrays
-	free(A);
-	cudaFree(AGPU);
-	free(D);
-	cudaFree(DGPU);
-	free(S);
-	cudaFree(SGPU);
+	free(diag);
+	cudaFree(diagGPU);
+	free(sub);
+	cudaFree(subGPU);
+	free(X);
+	cudaFree(XGPU);
 	free(Y);
 	cudaFree(YGPU);
 
