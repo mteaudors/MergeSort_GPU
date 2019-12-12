f3f4ddfce7104b17765b925ec92d06fd30e9f7b5
Mickael <mteaudors@outlook.com>
2019-12-12 10:51:20 +0100

    update batch

--- merge_batch.cu
+++ merge_batch.cu
@@ -1,38 +1,48 @@
 #include "cuda_header.h"
 
-__global__ void mergeSmallBatch_k(int *A, int length_A, int *B, int length_B, int *M) {
-        int d = length_A+length_B,
-            t = threadIdx.x,                        // index of the thread inside a block : 0 -> blockDim.x - 1
-            tidx = t%d,                             // index of the thread in its corresponding final array : 0 -> d-1
-            qt = (t-tidx)/d,                        // index of the group of the thread inside a block : 0 -> (blockDim.x/d)-1
-            gbx = (blockDim.x/d)*blockIdx.x + qt,   // index of the group of the thread among all the blocks : 0 -> (blockDim.x/d)*gridDim.x - 1
-            begin_M = gbx*d,                        // index of the first element of M
-            begin_A = gbx*length_A,                 // index of the first element of A
-            begin_B = gbx*length_B;                 // index of the first element of B
+/*
+	GPU kernel to merge, using a batch method, multiple couples of array (a,b).
+*/
+__global__ void mergeSmallBatch_k(int *AB, int length_A, int length_B, int *M, int nb_merge) {
+	int nb_threads = gridDim.x * blockDim.x,
+		gtidx = threadIdx.x + blockIdx.x * blockDim.x,
+		d = length_A+length_B;
+	
+	int t, tidx, qt, gbx, begin_M, begin_A, begin_B;
 
+	duo K, P, Q;
 
-        duo K, P, Q;
-        if (tidx > length_A) {
-                K.x = tidx - length_A;
-                K.y = length_A;
-                P.x = length_A;
-                P.y = tidx - length_A;
-        }else {
-                K.x = 0;
-                K.y = tidx;
-                P.x = tidx;
-                P.y = 0;
-        }
-        while (true) {
-                int offset = abs(K.y - P.y) / 2;
-                Q.x = K.x + offset;
-                Q.y = K.y - offset;
-                if (Q.y >= 0 && Q.x <= length_B && (Q.y == length_A || Q.x == 0 || A[begin_A + Q.y] > B[begin_B + Q.x - 1])) {
-                        if (Q.x == length_B || Q.y == 0 || A[begin_A + Q.y - 1] <= B[begin_B + Q.x]) {
-                                if (Q.y < length_A && (Q.x == length_B || A[begin_A + Q.y] <= B[begin_B + Q.x])) {
-                                        M[begin_M + tidx] = A[begin_A + Q.y];
-                                }else {
-                                        M[begin_M + tidx] = B[begin_B + Q.x];
+	while(gtidx < nb_merge*d) {
+		
+		t = gtidx % blockDim.x,                   		// index of the thread inside a block : 0 -> blockDim.x - 1
+		tidx = t%d,                             		// index of the thread in its corresponding final array : 0 -> d-1
+		qt = (t-tidx)/d,                        		// index of the group of the thread inside a block : 0 -> (blockDim.x/d)-1
+		gbx = (blockDim.x/d)*blockIdx.x + qt,   		// index of the group of the thread among all the blocks : 0 -> (blockDim.x/d)*gridDim.x - 1
+		begin_M = gbx*d,                        		// index of the first element of M
+		begin_A = gbx*d,                 				// index of the first element of A
+		begin_B = begin_A + length_A;                 	// index of the first element of B
+
+		if (tidx > length_A) {
+				K.x = tidx - length_A;
+				K.y = length_A;
+				P.x = length_A;
+				P.y = tidx - length_A;
+		}else {
+				K.x = 0;
+				K.y = tidx;
+				P.x = tidx;
+				P.y = 0;
+		}
+		while (true) {
+				int offset = abs(K.y - P.y) / 2;
+				Q.x = K.x + offset;
+				Q.y = K.y - offset;
+				if (Q.y >= 0 && Q.x <= length_B && (Q.y == length_A || Q.x == 0 || AB[begin_A + Q.y] > AB[begin_B + Q.x - 1])) {
+						if (Q.x == length_B || Q.y == 0 || AB[begin_A + Q.y - 1] <= AB[begin_B + Q.x]) {
+								if (Q.y < length_A && (Q.x == length_B || AB[begin_A + Q.y] <= AB[begin_B + Q.x])) {
+										M[begin_M + tidx] = AB[begin_A + Q.y];
+								}else {
+										M[begin_M + tidx] = AB[begin_B + Q.x];
 								}
 								break;
 						}
@@ -46,6 +56,8 @@ __global__ void mergeSmallBatch_k(int *A, int length_A, int *B, int length_B, in
 				P.y = Q.y + 1;
 				}
 		}
+		gtidx += nb_threads;
+	}
 }
 
 
